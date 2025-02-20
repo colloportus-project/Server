7ae44887f9442cb6b51be9d71f39d45cd3e00b81
hanwoooo <dngur515@naver.com>
2025-02-21 00:05:04 +0900

    delete some info

--- server/server/settings.py
+++ server/server/settings.py
@@ -90,17 +90,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'server.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
